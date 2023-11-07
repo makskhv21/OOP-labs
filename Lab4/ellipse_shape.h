e85f3d6bbecb0d55c73dea1af649c379d9b12107
makskhv21 <113525627+makskhv21@users.noreply.github.com>
2023-11-07 11:20:56 +0200

    Delete Lab4 directory

--- Lab4/ellipse_shape.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "shape.h"
-class EllipseShape : virtual public Shape {
-public:
-	virtual void Show(HDC);
-	void DrawRubber(HWND);
-	Shape* copyShape();
-	LPCTSTR setWindowText();
-};
--- Lab4/point_shape.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "shape.h"
-class PointShape : virtual public Shape {
-public:
-	void Show(HDC);
-	void DrawRubber(HWND);
-	Shape* copyShape();
-	LPCTSTR setWindowText();
-};
